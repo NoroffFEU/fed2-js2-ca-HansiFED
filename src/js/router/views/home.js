bcd3d6f60243145add7ddd537498dae0b5049a95
Haze <117860981+HansiFED@users.noreply.github.com>
2024-09-24 14:46:04 +0200

    Feat - User can now logout

--- src/js/router/views/home.js
+++ src/js/router/views/home.js
@@ -1,3 +1,8 @@
+import { onLogout } from "../../ui/auth/logout";
 import { authGuard } from "../../utilities/authGuard";
+
+const button = document.getElementById("logout-button");
+
+button.addEventListener("click", onLogout);
 
 authGuard();
